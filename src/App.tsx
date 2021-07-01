dba1c6795ed31b9aa68fac81f568839bdb493c56
Thainá Weingartner <65509827+thainaweingartner@users.noreply.github.com>
2021-07-01 19:45:17 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AuthContextProvider } from './contexts/AuthContext'
 import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" exact component={Home} /> 
           <Route path="/rooms/new" component={NewRoom} /> 
           <Route path="/rooms/:id" component={Room} /> 
+          <Route path="/admin/rooms/:id" component={AdminRoom} /> 
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,105 @@
+import { useHistory, useParams } from "react-router-dom"
+import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
+import { Button } from '../components/Button';
+import { Question } from "../components/Question";
+import { RoomCode } from '../components/RoomCode';
+import { useRoom } from "../hooks/useRoom";
+import { database } from "../services/firebase";
+import '../styles/room.scss';
+
+type RoomParams = {
+  id: string;
+}
+
+export function AdminRoom() {
+  const history = useHistory();
+  const { id } = useParams<RoomParams>();
+  const { title, questions } = useRoom(id)
+
+  async function handleEndRoom() {
+    if (window.confirm('Tem certeza que você deseja encerrar está sala?')) {
+      await database.ref(`rooms/${id}`).update({
+        endAt: new Date(),
+      });
+      history.push('/')
+    }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${id}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${id}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
+  }
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      await database.ref(`rooms/${id}/questions/${questionId}`).remove();
+    }
+  }
+
+ return (
+   <div id="page-room">
+     <header>
+      <div className="content">
+        <img src={logoImg} alt="Letmeask" />
+        <div>
+          <RoomCode code={id}/>
+          <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
+        </div>
+      </div>
+     </header>
+     <main className="content">
+      <div className="room-title">
+        <h1>{title}</h1>
+        {questions.length > 0 && <span>{questions.length} Pergunta(s)</span>}
+      </div>
+
+      {questions.map(question => {
+        return (
+          <Question
+            key={question.id}
+            content={question.content}
+            author={question.author}
+            isAnswered={question.isAnswered}
+            isHighlighted={question.isHighlighted}
+          >
+            <button
+              className="like-button"
+              type="button"
+              aria-label="Marcar como gostei"
+              onClick={() => handleCheckQuestionAsAnswered(question.id)}
+            >
+              <img src={checkImg} alt="highlight mensagem" />
+            </button>
+            <button
+              className="like-button"
+              type="button"
+              aria-label="Marcar como gostei"
+              onClick={() => handleHighlightQuestion(question.id)}
+            >
+              <img src={answerImg} alt="responder mensagem" />
+            </button>
+            <button
+              className="like-button"
+              type="button"
+              aria-label="Marcar como gostei"
+              onClick={() => handleDeleteQuestion(question.id)}
+            >
+              <img src={deleteImg} alt="deletar mensagem" />
+            </button>
+          </Question>
+        )
+      })}
+     </main>
+   </div>
+ )
+}
